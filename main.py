078ad5bf1087f57ae81288a64b7c8e5287b67ddf
tacheraSasi <tacherasasi@gmail.com>
2024-06-11 11:24:24 +0300

    setting up the project

---
+++ main.py
@@ -0,0 +1,13 @@
+def print_(name):
+    print(f"Hello, {name}")
+
+def get_name():
+    name = input("Please enter you name here: ")
+    return name
+
+print("Welcome, this Tach from ekilie")
+
+def main():
+    print_(get_name())
+#calling the main function 
+main()
